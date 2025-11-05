917ffd90928559bed95ccd08775f4ab32431dcc9
wifiisokay <lexuanthanh1234@gmail.com>
2025-11-05 23:07:52 +0700

    Merge branch seller

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // API Base URL
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/api/';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api/';
 
 // API Endpoints
 export const API_ENDPOINTS = {
